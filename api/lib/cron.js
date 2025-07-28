67ccc80aaf84b830f2486d66522675efa0a08cb7
surya <swainsuryakanta97@gmail.com>
2025-07-28 22:40:48 +0530

    cron updated

--- api/lib/cron.js
+++ api/lib/cron.js
@@ -2,11 +2,11 @@ import cron from "cron";
 import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *",()=>{
-    https.get("/tobeadded", (res) =>{
+    https.get("https://backend-storyshort.onrender.com", (res) =>{
         if(res.statusCode == 200) console.log("Get Request Success by cron");
         else console.log("Get Request failed by cron")
     })
     .on("error",(e) => console.log("Error in cron jobs error ",e));
 })
 
-export default job ;
+export default job ; 
